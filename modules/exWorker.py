d025101c9bbb952704eabaabeb38cbe3c6e0f48e
Yourandao <yoyrandao@gmail.com>
2018-12-24 00:45:55 +0300

    [UPD]: added GetWeek, SetWeek functions, current day class, GetOdd, GetEven funcs(modify) and constants, deleted shedule.py

--- modules/exWorker.py
+++ modules/exWorker.py
@@ -3,9 +3,34 @@ import pandas
 import openpyxl as xlReader
 import math
 
+C_EN_ALPHABET_CAP = 26
+C_ASCII_A_KEY = 65
+C_EXCEL_WEEK_LEN = 73
+C_DAY_SUBJS = 12
+
+class workDay:
+    subjects = []
+
+    def __init__(self, rowsArr):
+        self.subjects = rowsArr.copy()
+
+    def GetEven(self):
+        [print(self.subjects[i]) for i in range(0, len(self.subjects), 2)]
+
+    def GetOdd(self):
+        [print(self.subjects[i]) for i in range(1, len(self.subjects), 2)]
+
+
 class exWorker:
     sheetName = 'Лист1'
     curGroup = ''
+
+    coordinates = tuple()
+
+    weekData = []
+    workDays = []
+
+    days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 
     def __init__(self, exFile):
         self.exFile = exFile
@@ -18,18 +43,55 @@ class exWorker:
             for jCol in range(len(self.data[iRow])):
                 if(str(self.data[iRow][jCol]).startswith(group)):
                     self.curGroup = group
-                    return self.CoordToKey(jCol, iRow)
+                    self.coordinates = iRow, jCol, self.CoordToKey(jCol, iRow)
+
+                    return True
 
         return False
 
     def CoordToKey(self, jIndex, iIndex):
-        if jIndex >= 26:
-            remain = jIndex // 26
-            first = chr(64 + remain)
-            second = chr(65 + jIndex - remain * 26)
+        if jIndex >= C_EN_ALPHABET_CAP:
+            remain = jIndex // C_EN_ALPHABET_CAP
+            first = chr(C_ASCII_A_KEY - 1 + remain)
+            second = chr(C_ASCII_A_KEY + jIndex - remain * C_EN_ALPHABET_CAP)
             return str(first) + str(second) + str(iIndex + 1)
 
         else:
-            return str(chr(64 + jIndex)) + str(iIndex)
+            return str(chr(C_ASCII_A_KEY - 1 + jIndex)) + str(iIndex)
+
+
+    def SetWeek(self):
+        rowCount = 0
+
+        subjRows = []
+        for row in self.mainSheet[self.CoordToKey(self.coordinates[1], self.coordinates[0] + 2) :
+                                        self.CoordToKey(self.coordinates[1] + 3, self.coordinates[0] + C_EXCEL_WEEK_LEN)]:
+            sRow = ''
+            for elem in row:
+                self.weekData.append(elem.value)
+
+                if elem.value is None:
+                    sRow += str(' ')
+                else:
+                    sRow += str(elem.value) + ' '
+            
+            rowCount += 1
+            subjRows.append(sRow)
+
+            if rowCount == C_DAY_SUBJS:
+                rowCount = 0
+
+                self.workDays.append(workDay(subjRows))
+                subjRows.clear()
 
     
+    def GetWeek(self):
+        for dayIndex in range(len(self.workDays)):
+            print(self.days[dayIndex] + ':')
+
+            for subj in self.workDays[dayIndex].subjects:
+                if str(subj).replace(' ', '') != '':
+                    print(str(subj).replace('\n', ''))
+                    
+            print('------------------------------------')
+        
--- unibot.py
+++ unibot.py
@@ -5,14 +5,10 @@ import openpyxl         as xlReader
 import modules.exWorker as exHandle
 
 worker = exHandle.exWorker('asda.xlsx')
-# print(worker.exists('БСБО-09-17'))
 
-# for elem in worker.mainSheet['CD2:CG15']:
-#     for el in elem:
-#         if el.value is not None:
-#             print(el.value)
-#     print('----')
+worker.Exists('БСБО-09-17')
+worker.SetWeek()
 
-print(worker.Exists('БСБО-09-17'))
-
+# worker.days[2].GetEven()
+worker.GetWeek()
 
