c510eabb392054c570136dcfc36c42f9a1a24261
Yourandao <yoyrandao@gmail.com>
2018-12-30 14:45:42 +0300

    [UPD]: added final functions to excelParser and tested in discord. Setuped a simple discordBot commands and added youtube video playing. Downloaded: ffmpeg, youtube-dl, asyncio

--- modules/exWorker.py
+++ modules/exWorker.py
@@ -1,8 +1,10 @@
 import os
+import re
+import math
+import datetime
+
 import pandas
 import openpyxl as xlReader
-import math
-import re
 import requests
 
 C_EN_ALPHABET_CAP = 26
@@ -10,37 +12,47 @@ C_ASCII_A_KEY = 65
 C_EXCEL_WEEK_LEN = 73
 C_DAY_SUBJS = 12
 
-class workDay:
+A_WEEK_DAYS = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
+
+class WorkDay:
     subjects = []
 
-    def __init__(self, rowsArr):
+    def __init__(self, rowsArr, dayIndex):
         self.subjects = rowsArr.copy()
+        self.dayIndex = dayIndex
 
     def GetEven(self):
-        [print(str(i // 2 + 1) + ' пара: ' + str(self.subjects[i]).replace('\n', ''))
-            for i in range(0, len(self.subjects), 2) if not str(self.subjects[i]).isspace()]
+        rString = '\n, '
+
+        return A_WEEK_DAYS[self.dayIndex] + ':\n' + rString.join(
+            [str(i // 2 + 1) + ' пара: ' + str(self.subjects[i]).replace('\n', '')
+                for i in range(0, len(self.subjects), 2) if not str(self.subjects[i]).isspace()]
+            ).replace(',' , '').replace('*', ' ')
 
     def GetOdd(self):
-        [print(str(i // 2 + 1) + ' пара: ' + str(self.subjects[i]).replace('\n', ''))
-            for i in range(1, len(self.subjects), 2) if not str(self.subjects[i]).isspace()]
+        rString = '\n, '
+
+        return A_WEEK_DAYS[self.dayIndex] + ':\n' + rString.join(
+            [str(i // 2 + 1) + ' пара: ' + str(self.subjects[i]).replace('\n', '')
+                for i in range(1, len(self.subjects), 2) if not str(self.subjects[i]).isspace()]
+            ).replace(',', '').replace('*', ' ')
 
 
-class exWorker:
-    sheetName = 'Лист1'
+class ExWorker:
     curGroup = ''
 
-    coordinates = tuple()
-
-    workDays = []
-    days = ['понедельник', 'пторник', 'среда', 'четверг', 'пятница', 'суббота']
-
     def __init__(self, exFile):
+        self.sheetName = 'Лист1'
+        self.coordinates = tuple()
+        
         self.exFile = exFile
-        self.xlsx = xlReader.load_workbook('./' + exFile)
+        self.xlsx = xlReader.load_workbook('./shedules/' + exFile)
         self.mainSheet = self.xlsx.get_sheet_by_name(str(self.sheetName))
         self.data = list(self.mainSheet.values)
+        self.workDays = []
+        
 
-    def Exists(self, group):
+    def ParseIfExist(self, group):
         for iRow in range(len(self.data)):
             for jCol in range(len(self.data[iRow])):
                 if(str(self.data[iRow][jCol]).startswith(group)):
@@ -48,8 +60,11 @@ class exWorker:
                     self.curGroup = group
                     self.coordinates = iRow, jCol, self.CoordToKey(jCol, iRow)
 
+                    self.SetWeek()
+
                     return True
         return False
+
 
     def CoordToKey(self, jIndex, iIndex):
         if jIndex >= C_EN_ALPHABET_CAP:
@@ -66,6 +81,7 @@ class exWorker:
 
     def SetWeek(self):
         rowCount = 0
+        dayCount = 0
 
         subjRows = []
         for row in self.mainSheet[self.CoordToKey(self.coordinates[1], self.coordinates[0] + 2) :
@@ -83,32 +99,59 @@ class exWorker:
             if rowCount == C_DAY_SUBJS:
                 rowCount = 0
 
-                self.workDays.append(workDay(subjRows))
+                self.workDays.append(WorkDay(subjRows, dayCount))
                 subjRows.clear()
 
+                dayCount += 1
     
     def GetWeek(self):
         curWeek = self.GetWeekNumber() % 2
 
+        weekShedule = []
+
         for dayIndex in range(len(self.workDays)):
-            print(self.days[dayIndex] + ':')
+            dayResult = A_WEEK_DAYS[dayIndex] + ':\n'
 
             for i in range((0  if curWeek == 0 else 1), len(self.workDays[dayIndex].subjects), 2):
                 if not str(self.workDays[dayIndex].subjects[i]).isspace():
-                    print(str((i + 1) // 2 + (1 if curWeek == 0 else 0)) + ' пара: ' + 
-                        str(self.workDays[dayIndex].subjects[i]).replace('\n', ''))
+                    dayResult += str((i + 1) // 2 + (1 if curWeek == 0 else 0)) + ' пара: ' + str(self.workDays[dayIndex].subjects[i]).replace('\n', '') + '\n'
+            
+            weekShedule.append(dayResult)
+            dayResult = ''
+        
+        return '\n'.join(weekShedule).replace(',', '')
 
-            print('------------------------------------')
-    
     def GetDay(self, day):
         for i in range(len(self.workDays)):
-            if self.days[i] == str(day).lower():
-                self.workDays[i].GetEven() if self.GetWeekNumber() != 0 else self.workDays[i].GetOdd() # Don`t Touch
-
-                return True
+            if A_WEEK_DAYS[i] == str(day).lower():
+                return self.workDays[i].GetEven() if self.GetWeekNumber() % 2 != 0 else self.workDays[i].GetOdd() # Don`t Touch
                 
-        print('Пары не найдены')
-        return False
+        return 'Пары не найдены'
+
+    def GetNext(self):
+        curDay = datetime.datetime.today().weekday()
+        nextDay = (curDay + 1) % len(self.workDays)
+
+        if nextDay + 1 == 6:
+            return A_WEEK_DAYS[nextDay] + ':\n' + self.workDays[nextDay].GetEven() if self.GetWeekNumber() % 2 != 0 else self.workDays[nextDay].GetOdd()
+        else:
+            return 'Пары не найдены'
+
+    def GetBack(self):
+        curDay = datetime.datetime.today().weekday()
+
+        if curDay == 0:
+            curDay = 6
+        
+        return A_WEEK_DAYS[curDay - 1] + ':\n' + self.workDays[curDay - 1].GetEven() if self.GetWeekNumber() % 2 != 0 else self.workDays[curDay - 1].GetOdd()
+
+    def GetToday(self):
+        curDay = datetime.datetime.today().weekday()
+
+        if curDay != 6:
+            return A_WEEK_DAYS[curDay] + ':\n' + self.workDays[curDay].GetEven() if self.GetWeekNumber() % 2 != 0 else self.workDays[curDay].GetOdd()
+        else:
+            return 'Пары не найдены'
 
     def GetWeekNumber(self):
         response = requests.get('https://www.mirea.ru/').text
--- unibot.py
+++ unibot.py
@@ -1,14 +1,70 @@
 import os
-import discord
+
 import pandas
+import asyncio
+import discord
+import youtube_dl
 import openpyxl         as xlReader
 import modules.exWorker as exHandle
 
-worker = exHandle.exWorker('asda.xlsx')
+from discord.ext import commands
 
-worker.Exists('БСБО-09-17')
-worker.SetWeek()
+#-----------------------------------
 
-# worker.days[2].GetEven()
-worker.GetDay('ухйня')
+dClient = commands.Bot(command_prefix = '$')
 
+players = {}
+
+@dClient.event
+async def on_ready():
+    print('Logged in as ' + dClient.user.name + ' ' + dClient.user.id)
+
+@dClient.command(pass_context = True)
+async def join(request):
+    channel = request.message.author.voice.voice_channel
+
+    if str(channel) != 'None':
+        await dClient.join_voice_channel(channel)
+    else:
+        print('Nobody in channel')
+
+@dClient.command(pass_context = True)
+async def leave(request):
+    for client in dClient.voice_clients:
+        if client.server == request.message.server:
+            return await client.disconnect()
+
+@dClient.command(pass_context = True)
+async def play(request, url):
+    server = request.message.server
+    channel = request.message.author.voice.voice_channel
+
+    if not dClient.is_voice_connected(server):
+        if str(channel) != 'None':
+            vc = await dClient.join_voice_channel(channel)
+        else:
+            print('Channel is empty')
+    else:
+        vc = dClient.voice_client_in(server)
+
+    player = await vc.create_ytdl_player(url)
+    players[server.id] = player
+
+    player.start()
+
+@dClient.command(pass_context = True)
+async def pause(request):
+    players[request.message.server.id].pause()
+
+@dClient.command(pass_context = True)
+async def resume(request):
+    players[request.message.server.id].resume()
+
+@dClient.command(pass_context = True)
+async def stop(request):
+    players[request.message.server.id].stop()
+
+#-----------------------------------
+
+D_TOKEN = open('./stuff/D_TOKEN.txt', 'r').read()
+dClient.run(D_TOKEN)
