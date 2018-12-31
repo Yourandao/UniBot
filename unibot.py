e35eebb0d9a7615f23ab6f29ea9f3259ed3e4bcb
Yourandao <yoyrandao@gmail.com>
2018-12-31 14:29:37 +0300

    [UPD]: added queue function to store the players

--- unibot.py
+++ unibot.py
@@ -14,6 +14,7 @@ from discord.ext import commands
 dClient = commands.Bot(command_prefix = '$')
 
 players = {}
+queues   = {}
 
 @dClient.event
 async def on_ready():
@@ -47,7 +48,7 @@ async def play(request, url):
     else:
         vc = dClient.voice_client_in(server)
 
-    player = await vc.create_ytdl_player(url)
+    player = await vc.create_ytdl_player(url, after= lambda: checkQueue(server.id))
     players[server.id] = player
 
     player.start()
@@ -63,6 +64,25 @@ async def resume(request):
 @dClient.command(pass_context = True)
 async def stop(request):
     players[request.message.server.id].stop()
+
+@dClient.command(pass_context = True)
+async def queue(request, url):
+    server = request.message.server
+    voiceClient = dClient.voice_client_in(server)
+    player = await voiceClient.create_ytdl_player(url, after = lambda: checkQueue(server.id))
+
+    if server.id in queues:
+        queues[server.id].append(player)
+    else:
+        queues[server.id] = [player]
+    
+    await dClient.say('Video queued')
+
+def checkQueue(serverID):
+    if queues[serverID] != []:
+        player = queues[serverID].pop(0)
+        players[serverID] = player
+        player.start()
 
 #-----------------------------------
 
